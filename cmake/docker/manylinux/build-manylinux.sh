659218ac5896eaafb72a2acaeb4861336e977573
Mizux Seiha <mizux.dev@gmail.com>
2025-01-23 17:15:59 +0100

    cmake: Fix manylinux ci
    note: The goal of the workflows is to build bp11 using
    the pypa manylinux image then test that we can install
    and use it on a ubuntu:latest

--- cmake/docker/manylinux/build-manylinux.sh
+++ cmake/docker/manylinux/build-manylinux.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+
 # Build all the wheel artifacts for the platforms supported by manylinux2014 and
 # export them to the specified location.
 set -euxo pipefail
+
+function assert_defined(){
+  if [[ -z "${!1}" ]]; then
+    >&2 echo "Variable '${1}' must be defined"
+    exit 1
+  fi
+}
 
 function usage() {
   local -r NAME=$(basename "$0")
@@ -38,55 +46,82 @@ function contains_element() {
 }
 
 function build_wheel() {
+  assert_defined BUILD_DIR
+  assert_defined VENV_DIR
   # Build the wheel artifact
   # Arguments:
   #   $1 the python root directory
   if [[ "$#" -ne 1 ]]; then
     echo "$0 called with an illegal number of parameters"
-    exit 1  # TODO return error and check it outside
+    exit 1
   fi
 
-  #pwd
+  # Create and activate virtualenv
+  # this is needed so protoc can call the correct python executable
+  local -r PYBIN="$1/bin"
+  "${PYBIN}/pip" install virtualenv
+  "${PYBIN}/virtualenv" -p "${PYBIN}/python" "${VENV_DIR}"
+  # shellcheck source=/dev/null
+  source "${VENV_DIR}/bin/activate"
+  pip install -U pip setuptools wheel
+
+  echo "current dir: $(pwd)"
+
   if [[ ! -e "CMakeLists.txt" ]] || [[ ! -d "cmake" ]]; then
     >&2 echo "Can't find project's CMakeLists.txt or cmake"
     exit 2
   fi
+  cmake -S. -B"${BUILD_DIR}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPython3_ROOT_DIR="$1" \
+    -DBUILD_TESTING=OFF #--debug-find
+  cmake --build "${BUILD_DIR}" -v -j4
 
-  cmake -S. -B"${BUILD_DIR}" -DPython3_ROOT_DIR="$1" -DBUILD_TESTING=OFF #--debug-find
-  cmake --build "${BUILD_DIR}"
+  # Restore environment
+  deactivate
 }
 
 function check_wheel() {
+  assert_defined BUILD_DIR
+  assert_defined VENV_DIR
   # Check the wheel artifact
   # Arguments:
   #   $1 the python root directory
   if [[ "$#" -ne 1 ]]; then
     echo "$0 called with an illegal number of parameters"
-    exit 1  # TODO return error and check it outside
+    exit 1
   fi
 
-  # Build python bindings
+  # shellcheck source=/dev/null
+  source "${VENV_DIR}/bin/activate"
+  pip install -U auditwheel
+
+  # Check all generated wheel packages
+  ROOT_DIR=$(pwd)
   pushd "${BUILD_DIR}/python/dist"
   for FILE in *.whl; do
     # if no files found do nothing
     [[ -e "$FILE" ]] || continue
-    auditwheel show "$FILE" || true
-    #auditwheel -v repair --plat manylinux2014_aarch64 "$FILE" -w "$export_root"
+    python -m auditwheel show "$FILE" || true
+    python -m auditwheel repair --plat "manylinux_2_28_x86_64" "$FILE" -w .
+    #python -m auditwheel -v repair --plat manylinux_2_28_x86_64 "$FILE" -w .
   done
   popd
+
+  # Restore environment
+  deactivate
 }
 
 function test_wheel() {
+  assert_defined BUILD_DIR
+  assert_defined TEST_DIR
   # Test the wheel artifacts
   # Arguments:
   #   $1 the python root directory
   if [[ "$#" -ne 1 ]]; then
     echo "$0 called with an illegal number of parameters"
-    exit 1  # TODO return error and check it outside
+    exit 1
   fi
-
-  local -r BUILD_DIR="build_${PYTAG}"
-  local -r TEST_DIR="test_${PYTAG}"
 
   # Create and activate virtualenv
   local -r PYBIN="$1/bin"
@@ -102,29 +137,31 @@ function test_wheel() {
   local -r WHEEL_FILE=$(find "${BUILD_DIR}"/python/dist/*.whl | head -1)
   echo "WHEEL file: ${WHEEL_FILE}"
   pip install --no-cache-dir "$WHEEL_FILE"
-  pip show pythonnative
+  pip show bp11
 
-  # Run tests
-  run_tests
+  # Python scripts to be used as tests for the installed wheel. This list of files
+  # has been taken from the 'test_python' make target.
+  declare -a TESTS=(
+    "samples/sample.py"
+    "tests/test.py"
+  )
+
+  # Run all the specified test scripts using the current environment.
+  local -r ROOT_DIR=$(pwd)
+  pushd "$(mktemp -d)" # ensure we are not importing something from $PWD
+  python --version
+  for TEST in "${TESTS[@]}"; do
+    python "${ROOT_DIR}/${TEST}"
+  done
+  popd
 
   # Restore environment
   deactivate
 }
 
-function run_tests() {
-  # Run all the specified test scripts using the current environment.
-  pushd "$(mktemp -d)" # ensure we are not importing something from $PWD
-  python --version
-  for TEST in "${TESTS[@]}"
-  do
-    python "$TEST"
-  done
-  popd
-}
-
 function build() {
   # For each python platform provided by manylinux, build and test artifacts.
-  for PYROOT in /opt/python/*; do
+  for PYROOT in /opt/python/cp*; do
     # shellcheck disable=SC2155
     PYTAG=$(basename "$PYROOT")
     echo "Python: $PYTAG"
@@ -136,7 +173,7 @@ function build() {
     fi
 
     BUILD_DIR="build_${PYTAG}"
-    TEST_DIR="test_${PYTAG}"
+    VENV_DIR="env_${PYTAG}"
     build_wheel "$PYROOT"
     check_wheel "$PYROOT"
   done
@@ -144,7 +181,7 @@ function build() {
 
 function tests() {
   # For each python platform provided by manylinux, build and test artifacts.
-  for PYROOT in /opt/python/*; do
+  for PYROOT in /opt/python/cp*; do
     # shellcheck disable=SC2155
     PYTAG=$(basename "$PYROOT")
     echo "Python: $PYTAG"
@@ -154,9 +191,9 @@ function tests() {
       >&2 echo "skipping deprecated platform $PYTAG"
       continue
     fi
+
     BUILD_DIR="build_${PYTAG}"
     TEST_DIR="test_${PYTAG}"
-
     test_wheel "$PYROOT"
   done
 }
@@ -169,10 +206,7 @@ function main() {
   esac
 
   # Setup
-  # Python scripts to be used as tests for the installed wheel. This list of files
-  # has been taken from the 'test_python' make target.
-  declare -a TESTS=( "/home/project/ci/samples/sample.py" "/home/project/tests/test.py" )
-  declare -a SKIPS=( "pp37-pypy37_pp73" )
+  declare -a SKIPS=("cp36-cp36m" "cp37-cp37m")
 
   case ${1} in
     build)
