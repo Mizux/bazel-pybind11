7dfd14dd7a702ef0f5867a88a096e4fb70f28bd6
Mizux Seiha <mizux.dev@gmail.com>
2025-01-23 21:03:49 +0100

    pure_lib: rework

--- pure_lib/mylib.py
+++ pure_lib/mylib.py
@@ -1,15 +1,19 @@
-'''Plop module '''
+"""MyLib module """
 
 import sys
 
-def free_function():
-    print(f'plop_function')
-    print(f'version_info: {sys.version_info}')
-    return 42
 
-class Plop:
+def free_function(i=42):
+    print(f"free_function")
+    print(f"version_info: {sys.version_info}")
+    return i
+
+
+class MyLib:
     """A simple example class"""
-    i = 12345
 
-    def f(self):
-        return 31
+    def __init__(self, i=12345):
+        self.i = i
+
+    def method(self, i=0):
+        return i + self.i
--- pure_lib/mylib_test.py
+++ pure_lib/mylib_test.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python3
-'''Test APIs'''
+"""Test APIs"""
 
 import sys
 import unittest
 
 import pure_lib
 import pure_lib.mylib as mylib
-from pure_lib.mylib import Plop
+from pure_lib.mylib import MyLib
 
 if __debug__:
-    print(f'python path: {sys.path}')
+    print(f"python path: {sys.path}")
 
-    print(f'plop.python: ${dir(pure_lib.mylib)}')
+    print(f"plop.python: ${dir(pure_lib.mylib)}")
 
-    print(f'plop.python.mylib: ${dir(pure_lib.mylib)}')
-    print(f'mylib: ${dir(mylib)}')
+    print(f"plop.python.mylib: ${dir(pure_lib.mylib)}")
+    print(f"mylib: ${dir(mylib)}")
 
-    print(f'plop.python.mylib.Plop: ${dir(pure_lib.mylib.Plop)}')
-    print(f'mylib.Plop: ${dir(mylib.Plop)}')
+    print(f"plop.python.mylib.MyLib: ${dir(pure_lib.mylib.MyLib)}")
+    print(f"mylib.MyLib: ${dir(mylib.MyLib)}")
 
-class TestPlop(unittest.TestCase):
-    '''Test Plop'''
+
+class TestMyLib(unittest.TestCase):
+    """Test MyLib"""
+
     def test_free_function(self):
         ret = mylib.free_function()
         self.assertEqual(42, ret)
 
     def test_class(self):
-        p = Plop()
-        self.assertEqual(31, p.f())
+        p = MyLib(31)
+        self.assertEqual(31, p.method())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main(verbosity=2)
